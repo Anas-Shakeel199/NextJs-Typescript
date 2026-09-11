0a76bedade08faf6b48fa2cc82b39b0354aadcd9
Anas Chaudhry <anas.ch199786@gmail.com>
2026-09-12 01:30:07 +0500

    Data fateching in next js SSR, SSg ISR

--- level4/src/app/page.tsx
+++ level4/src/app/page.tsx
@@ -1,6 +1,38 @@
+import { log } from 'console'
 import React from 'react'
 
-function page() {
+async function page() {
+
+  // ssr
+  let response = await fetch("http://localhost:3000/api/user", {
+    cache: "no-store"
+  })
+  let data = await response.json()
+  console.log(data)
+
+  //ssg Bydefault ya ssg he use kerta han 
+  let response2 = await fetch("http://localhost:3000/api/user", {
+    cache: "force-cache"
+  })
+  let data2 = await response2.json()
+  console.log(data2)
+
+
+  //ssg Bydefault ya ssg he use kerta han 
+  let response1 = await fetch("http://localhost:3000/api/user", {
+    cache: "force-cache"
+  })
+  let data1 = await response1.json()
+  console.log(data1)
+
+   //ssg Bydefault ya ssg he use kerta han 
+  let response3 = await fetch("http://localhost:3000/api/user", {
+    next: {revalidate: 10}// 10 sec baad ya automicallty api call kara ga 
+  })
+  let data3 = await response3.json()
+  console.log(data3)
+
+
   return (
     <div>page</div>
   )
